e889d47990a3c5cc16b16eca6d12fd113779077d
Mike Botsko <botsko@gmail.com>
2019-02-14 13:42:45 -0800

    Improves arrow function formatting

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ const postcss = require('postcss');
 const through2 = require('through2');
 
 module.exports = function(opts) {
-    return () => {
-        return through2.obj(function(file, encoding, cb) {
-            if (file.isNull()) {
-                // nothing to do
-                return cb(null, file);
+    return () => through2.obj(function(file, encoding, cb) {
+        if (file.isNull()) {
+            // nothing to do
+            return cb(null, file);
+        }
+
+        postcss(opts).process(file.contents, {
+            from: file.name
+        }).then((result) => {
+            file.contents = Buffer.from(result.css);
+
+            if (result.error) {
+                this.emit('spearhook:error', {
+                    error: result.error,
+                    file
+                });
             }
 
-            postcss(opts).process(file.contents, {
-                from: file.name
-            }).then((result) => {
-                file.contents = Buffer.from(result.css);
+            cb(null, file);
+        }).catch(error => {
+            this.emit('spearhook:error', { err, file });
 
-                if (result.error) {
-                    this.emit('spearhook:error', {
-                        error: result.error,
-                        file
-                    });
-                }
-
-                cb(null, file);
-            }).catch(error => {
-                this.emit('spearhook:error', { err, file });
-
-                cb(null, file);
-            });
+            cb(null, file);
         });
-    }
+    });
 };
